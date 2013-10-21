c044093b4482b30d8a4e558106b5a9e91b3846b3
Avi Bryant <avi@avibryant.com>
2013-10-21 00:30:27 -0700

    document a possible wire format

--- lib/message.rb
+++ lib/message.rb
@@ -15,3 +15,19 @@ module Rumour
     end
   end
 end
+
+=begin
+possible wire format:
+1 byte: TTL
+2 bytes: key size
+key
+1 byte: vector format
+2 bytes: vector size
+vector
+
+Vector formats:
+2-byte sparse (10 bits of index, 6 bits of value)
+6-byte sparse (2 bytes of index, 4 bytes of value)
+1-byte dense
+4-byte dense
+=end
